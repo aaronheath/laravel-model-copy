59386330aabde0f23482129f9dbfb8eff704bc24
Aaron Heath <aaron@aaronheath.com>
2021-09-15 03:09:31 +0930

    wip

---
+++ tests/Models/ExampleB.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ExampleB extends Model
+{
+    protected $table = 'example_b';
+
+    protected $guarded = [];
+}
